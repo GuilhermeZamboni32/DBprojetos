236370a4e5ea63c6dd1fa9532be2774b5f7acf33
GuilhermeZamboni32 <guilhermezamboni867@gmail.com>
2025-05-19 16:46:51 -0300

    teste

--- db.js
+++ db.js
@@ -1,20 +1,47 @@
-require("dotenv").config(); // Ele procura o arquivo .env na raiz do projeto e carrega as variáveis ​​que estão no arquivo .env na memória.
+async function connect() {
+        const { Pool } = require("pg");
+    
+        if(global.connection)
+            return global.connection.connect();
+    
+        const pool = new Pool({
+            user: process.env.USER_NAME,
+            host: process.env.HOST_NAME,
+            database: process.env.DB_NAME,
+            password: process.env.DB_PASSWORD,
+            dialect: process.env.DB_DIALECT,
+            port: process.env.PORT_NUMBER
+        })
+        
+        const client = await pool.connect(); // Conectandooo
+        console.log("Connection pool created successfully!")
+    
+        const resdb = await client.query("SELECT now()");
+        console.log(resdb.rows[0]); // Tomando a primeira posição do array de onde virá o tempo do banco de dados.
+        client.release()
+    
+      // Podemos salvar nosso pool em uma conexão global. Então podemos executar o "if" como no início deste arquivo
+        global.connection = pool;
+    
+        return pool.connect()
+    }
+    
+    connect(); // Lembrando/Lembrete: temos que carregar/importar o arquivo db.js no nosso back-end index.js.
+    
+    async function insertCustomer(costmer){
+        //connecta
+        const client = await connect();
 
-const db = require("./db");
+        //consulta sql
+        const sql = "INSERT INTO cliente (cpf, nome, email, idade, profissao) VALUES ($1, $2, $3, $4, $5)";
 
-const port = process.env.PORT;
+        const values =[costmer.cpf, costmer.nome, costmer.email, costmer.idade, costmer.profissao];
+        
+        //agora sim nos vamos enviar od dados para o banco de dados
+        await client.query(sql, values);
 
-const express = require('express');
-
-const app = express();
-
-// como os dados que vamos inserir no banco de dados estão chegando no formato .json, temos que preparar o back-end para receber esses dados
-app.use(express.json());
-
-// app.get('caminho da rota', (função de callback que na verdade é a função que vai ser disparada quando a rota é chamada)=> {})
-
-
-
-app.listen(port);
-
-console.log("Backend is running")
+    }
+    //exportando cada função para que a gente consiga as usar no nosso bacend!!!
+    module.exports = {
+        insertCustomer
+    }
--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-require("dotenv").config();
+require("dotenv").config(); // Ele procura o arquivo .env na raiz do projeto e carrega as variáveis ​​que estão no arquivo .env na memória.
 
 const db = require("./db");
-
 const express = require('express');
-
 const mongoose = require('mongoose'); 
-
-
-const Cliente = require('./models/Cliente');
- 
-
-
+const Cliente = require('./models/cliente');
 const app = express();
-const port = process.env.PORT || 3000;
 
 
+// como os dados que vamos inserir no banco de dados estão chegando no formato .json, temos que preparar o back-end para receber esses dados
 app.use(express.json());
 
 
+const port = process.env.PORT || 3000;
+
+//rota para inserir clientes
 app.post('/clientes', async (req, res) => {
     try {
         const cliente = new Cliente(req.body);
@@ -29,6 +25,20 @@ app.post('/clientes', async (req, res) => {
 });
 
 
-app.listen(port, () => {
-    console.log(`Rodando o Backend na porta ${port}`);
+// app.get('caminho da rota', (função de callback que na verdade é a função que vai ser disparada quando a rota é chamada)=> {})
+app.get('/clientes', async (req, res) => {
+    try {
+        const clientes = await Cliente.find();
+        res.status(200).send(clientes);
+    } catch (err) {
+        res.status(500).send({ error: 'Erro ao buscar clientes', detalhes: err.message });
+    }
 });
+
+
+
+
+
+app.listen(port);
+
+console.log("Backend rodando na porta " + port);
